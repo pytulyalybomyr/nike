0374dd4486225e311692be22d558f75d9001ece2
pytulyalybomyr <pytulyalybomyr@gmail.com>
2023-09-15 12:46:48 +0300

    first commit

---
+++ js/burger.js
@@ -0,0 +1,9 @@
+$('.burger-close').click(function () {
+    $('.footer-burger__main').addClass("hide")
+    console.log("Sasasas")
+});
+
+$('.burger-open').click(function () {
+    $('.footer-burger__main').removeClass('hide');
+
+});
